5d6d4a931bed0830d68aa5ae389f553c9ccee09d
AlienGFX <kevin@rkweb.fr>
2017-02-08 20:31:49 +0100

    [api][config] add walletUser entry

--- api/config.template.php
+++ api/config.template.php
@@ -4,6 +4,7 @@ require_once('../vendor/block_io-php/block_io-php/lib/block_io.php');
 
 $apiKey = 'my_api_key';
 $pin = 'my_pin';
+$walletUser = 'my_wallet';
 $version = 2; // the API version
 
 $block_io = new BlockIo($apiKey, $pin, $version);
